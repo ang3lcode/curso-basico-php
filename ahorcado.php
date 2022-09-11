d6b64718fce4a1916b4a4f9682efd00c4f38629d
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-09-10 22:55:15 -0500

    ahorcado

---
+++ ahorcado.php
@@ -0,0 +1,54 @@
+<?php
+$possible_words = [
+    "Bebida","Prisma","Ala","Dolor","Piloto","Baldosa",
+    "Terremoto","Asteroide","Gallo","Platzi",
+];
+define("MAX_ATTEMPS", 6);
+echo "¡juego del ahorcado!\n\n"
+
+//inicilizamos el juego;
+$choosen_word = $possible_words[rand(0,9)]; // escoge la palabra
+$choosen_word = strtolower($choosen_word); // letras
+$word_length = strlen($choosen_word); //strlen , mide la longitud de la palabra
+$discovered_letters = str_pad(" ",$word_length, "_");
+$attempts = 0;
+
+do {
+    echo "Palabra de $word_length letras \n\n";
+    echo $discovered_letters . "\n\n";
+
+    // Pedimos al usuario que escriba
+    $player_letter = readline("Escribe una letra: ");
+    $player_letter = strtolower($player_letter);
+
+    if ( str_contains($choosen_word, $player_letter) ) {
+
+        // Verificamos todas las ocurrencias de esta letra para reemplazarla
+        $offset = 0;
+        while(
+            ($letter_position = strpos($choosen_word, $player_letter, $offset)) !== false
+        ) {
+            $discovered_letters[$letter_position] = $player_letter;
+            $offset = $letter_position + 1;
+        }        
+    } 
+    else {
+        clear();
+        $attempts++;
+        echo "Letra incorrecta 😾. Te quedan " . (MAX_ATTEMPS - $attempts) . " intentos.";
+        sleep(2);
+    }
+    clear();
+} while( $attempts < MAX_ATTEMPS && $discovered_letters != $choosen_word );
+
+clear();
+if ($attempts < MAX_ATTEMPS)
+    echo "¡Felicidades! Has adivinado la palabra. 🙀 \n\n";
+else
+    echo "Suerte para la próxima, amigo. 😾 \n\n";
+echo "La palabra es: $choosen_word\n";
+echo "Tú descubriste $discovered_letters";
+
+echo "\n";
+?>
+
